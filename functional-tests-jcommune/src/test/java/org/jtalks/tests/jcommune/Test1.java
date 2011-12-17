6deb8650496cd2b943be876abcc8d95aa8d0ea18
ctapobep <ctapobep@ctapobep-K52Jr.(none)>
2011-12-17 20:12:54 +0200

    structure for functional tests was created

---
+++ functional-tests-jcommune/src/test/java/org/jtalks/tests/jcommune/Test1.java
@@ -0,0 +1,17 @@
+package org.jtalks.tests.jcommune;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ctapobep
+ * Date: 12/17/11
+ * Time: 8:11 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test1 {
+    @Test
+    public void testNothing() throws Exception {
+
+    }
+}
