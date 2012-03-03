63a9bab7fc615a1c911143e3b1b77ccad082e272
masyan <jmasyan@gmail.com>
2012-03-03 21:31:13 +0600

    #JC-354

--- functional-tests-jcommune/src/test/java/org/jtalks/tests/jcommune/tests/post/JC18CancelButtonOnDeleteAnswerWindow.java
+++ functional-tests-jcommune/src/test/java/org/jtalks/tests/jcommune/tests/post/JC18CancelButtonOnDeleteAnswerWindow.java
@@ -44,7 +44,6 @@ public class JC18CancelButtonOnDeleteAnswerWindow {
 		//step 1
 		postPage.getDeleteButtonNearLastPost().click();
 		postPage.getDeleteConfirmCancelButton().click();
-
 		//step2
 		String lastMessageText = postPage.getLastPostMessage().getText();
 
