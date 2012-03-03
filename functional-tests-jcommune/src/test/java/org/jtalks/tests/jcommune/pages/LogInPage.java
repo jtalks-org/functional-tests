5a038fa46734639bfd4cdd5785459be56612756c
Erik Khalimov <biomaks@gmail.com>
2012-03-03 14:26:05 +0400

    #JC-355 added LogInPage

---
+++ functional-tests-jcommune/src/test/java/org/jtalks/tests/jcommune/pages/LogInPage.java
@@ -0,0 +1,57 @@
+package org.jtalks.tests.jcommune.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: erik
+ * Date: 03.03.12
+ * Time: 13:06
+ * To change this template use File | Settings | File Templates.
+ */
+public class LogInPage implements Page{
+
+    @FindBy(id = "j_username")
+    WebElement usernameField;
+
+    @FindBy (id = "j_password")
+    WebElement passwordField;
+
+    @FindBy(xpath = "//input[@class='button']")
+    WebElement submitButton;
+
+    @FindBy(xpath = "//input[@name='_spring_security_remember_me']")
+    WebElement rememberMeCheckBox;
+
+    @FindBy(xpath = "//div[@class='form_controls']/a[@href='/test-jcommune/password/restore']")
+    WebElement restorePasswordLink;
+
+
+
+    public void init(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+    public WebElement getUsernameField() {
+        return usernameField;
+    }
+
+    public WebElement getPasswordField() {
+        return passwordField;
+    }
+
+    public WebElement getSubmitButton() {
+        return submitButton;
+    }
+
+    public WebElement getRememberMeCheckBox() {
+        return rememberMeCheckBox;
+    }
+
+    public WebElement getRestorePasswordLink() {
+        return restorePasswordLink;
+    }
+}
