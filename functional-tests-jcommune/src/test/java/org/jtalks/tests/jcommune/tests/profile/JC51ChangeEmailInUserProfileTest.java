f7bad8cb1013650492155c671a05887a2d48c176
Erik Khalimov <biomaks@gmail.com>
2012-03-09 21:33:25 +0400

    #JC-391 created JC51 test

---
+++ functional-tests-jcommune/src/test/java/org/jtalks/tests/jcommune/tests/profile/JC51ChangeEmailInUserProfileTest.java
@@ -0,0 +1,55 @@
+package org.jtalks.tests.jcommune.tests.profile;
+
+import org.jtalks.tests.jcommune.pages.ProfilePage;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import utils.StringHelp;
+
+import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.*;
+
+/**
+ * author: erik
+ * Date: 09.03.12
+ * Time: 20:24
+ */
+public class JC51ChangeEmailInUserProfileTest {
+
+    ProfilePage profilePage;
+    String uniqEmail;
+    String currentEmail;
+
+
+    @BeforeMethod
+    @Parameters({"app-url", "uUsername", "uPassword", "uEmail"})
+    public void setupCase(String appUrl, String username, String password, String uEmail) {
+        driver.get(appUrl);
+        uniqEmail = StringHelp.getRandomEmail();
+        currentEmail = uEmail;
+        signIn(username, password);
+        profilePage = new ProfilePage(driver);
+        profilePage.getCurrentUserLink().click();
+        profilePage.getEditProfileButton().click();
+    }
+
+    @AfterMethod
+    @Parameters({"app-url"})
+    public void destroy(String appUrl) {
+        profilePage.getCurrentUserLink().click();
+        profilePage.getEditProfileButton().click();
+        profilePage.getEmailEditField().clear();
+        profilePage.getEmailEditField().sendKeys(currentEmail);
+        profilePage.getSaveEditButton().click();
+        logOut(appUrl);
+    }
+
+    @Test
+    public void testChangingEmail() {
+        profilePage.getEmailEditField().clear();
+        profilePage.getEmailEditField().sendKeys(uniqEmail);
+        profilePage.getSaveEditButton().click();
+        Assert.assertEquals(profilePage.getEmail().getText(), uniqEmail);
+    }
+}
