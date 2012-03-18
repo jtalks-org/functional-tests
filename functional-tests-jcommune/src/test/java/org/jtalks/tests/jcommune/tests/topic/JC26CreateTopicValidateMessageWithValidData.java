dc26781eb45eb23baf058a8a6024d4b086a0bcfe
Erik Khalimov <biomaks@gmail.com>
2012-03-18 10:50:34 +0400

    refactoring

---
+++ functional-tests-jcommune/src/test/java/org/jtalks/tests/jcommune/tests/topic/JC26CreateTopicValidateMessageWithValidData.java
@@ -0,0 +1,62 @@
+package org.jtalks.tests.jcommune.tests.topic;
+
+import org.jtalks.tests.jcommune.pages.TopicPage;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import utils.StringHelp;
+
+import static org.jtalks.tests.jcommune.Assert.Exsistence.assertExistBySelector;
+import static org.jtalks.tests.jcommune.Assert.Exsistence.assertNotExistBySelector;
+import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.*;
+
+
+/**
+ * author: erik
+ * Date: 08.03.12
+ * Time: 12:06
+ */
+public class JC26CreateTopicValidateMessageWithValidData {
+    TopicPage topicPage;
+
+    @DataProvider (name = "validTopic")
+    public Object[][] validTopic(){
+        String validTitle = "new topic";
+        String validTopicMessage = StringHelp.getRandomString(20000);
+        return new Object[][]{
+                {validTitle, validTopicMessage}
+        };
+    }
+
+	@BeforeMethod
+	@Parameters({"app-url", "uUsername", "uPassword"})
+	public void setupCase(String appUrl, String username, String password) {
+		driver.get(appUrl);
+		signIn(username, password);
+		clickOnRandomBranch();
+		topicPage = new TopicPage(driver);
+	}
+
+	@AfterMethod
+	@Parameters({"app-url"})
+	public void destroy(String appUrl) {
+		logOut(appUrl);
+	}
+
+	@Test(dataProvider = "validTopic")
+	public void testValidateMassage(String validTitle, String validTopicMessage) {
+		topicPage.getNewButton().click();
+		topicPage.getSubjectField().sendKeys(validTitle);
+		topicPage.getPostButton().click();
+
+		assertNotExistBySelector(driver, TopicPage.subjectErrorMessageSel);
+		assertExistBySelector(driver, TopicPage.bodyErrorMessageSel);
+
+		topicPage.getMessageField().clear();
+		StringHelp.setLongTextValue(driver, topicPage.getMessageField(), validTopicMessage);
+		topicPage.getPostButton().click();
+
+		Assert.assertEquals(topicPage.getTopicSubject().getText(), validTitle);
+		Assert.assertEquals(topicPage.getTopicMessage().getText(), validTopicMessage);
+	}
+
+}
