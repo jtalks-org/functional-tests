b7b352ffadd3e510f38c8a87afa2fc8cb66d702d
Erik Khalimov <biomaks@gmail.com>
2012-04-14 14:11:28 +0400

    little refactoring

--- functional-tests-jcommune/src/test/java/org/jtalks/tests/jcommune/tests/topic/JC13CreateTopic.java
+++ functional-tests-jcommune/src/test/java/org/jtalks/tests/jcommune/tests/topic/JC13CreateTopic.java
@@ -9,50 +9,47 @@ import org.testng.annotations.Test;
 import utils.StringHelp;
 
 import static org.jtalks.tests.jcommune.Assert.Exsistence.assertContainsInString;
-import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.clickOnRandomBranch;
-import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.driver;
-import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.logOut;
-import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.signIn;
+import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.*;
 
 /**
  * @autor masyan
  * @autor erik
  */
 public class JC13CreateTopic {
-	String subject = StringHelp.getRandomString(20);
-	String message = StringHelp.getRandomString(21);
-	TopicPage topicPage;
+    String topic_title = StringHelp.getRandomString(50);
+    String message = StringHelp.getRandomString(250);
+    TopicPage topicPage;
 
-	@BeforeMethod(alwaysRun = true)
-	@Parameters({"app-url", "uUsername", "uPassword"})
-	public void setupCase(String appUrl, String username, String password) {
-		driver.get(appUrl);
-		signIn(username, password);
-		clickOnRandomBranch();
-		topicPage = new TopicPage(driver);
-	}
+    @BeforeMethod(alwaysRun = true)
+    @Parameters({"app-url", "uUsername", "uPassword"})
+    public void setupCase(String appUrl, String username, String password) {
+        driver.get(appUrl);
+        signIn(username, password);
+        clickOnRandomBranch();
+        topicPage = new TopicPage(driver);
+    }
 
-	@AfterMethod(alwaysRun = true)
-	@Parameters({"app-url"})
-	public void destroy(String appUrl) {
-		logOut(appUrl);
-	}
+    @AfterMethod(alwaysRun = true)
+    @Parameters({"app-url"})
+    public void destroy(String appUrl) {
+        logOut(appUrl);
+    }
 
-	@Test
-	public void createTopicTest() {
-		//first step
-		topicPage.getNewButton().click();
+    @Test
+    public void createTopicTest() {
+        //first step
+        topicPage.getNewButton().click();
 
-		Assert.assertTrue(topicPage.getSubjectField().isDisplayed());
-		Assert.assertTrue(topicPage.getMessageField().isDisplayed());
+        Assert.assertTrue(topicPage.getSubjectField().isDisplayed());
+        Assert.assertTrue(topicPage.getMessageField().isDisplayed());
 
-		//second step
-		topicPage.getSubjectField().sendKeys(subject);
-		topicPage.getMessageField().sendKeys(message);
-		topicPage.getPostButton().click();
-		Assert.assertEquals(topicPage.getTopicSubject().getText(), subject);
+        //second step
+        topicPage.getSubjectField().sendKeys(topic_title);
+        topicPage.getMessageField().sendKeys(message);
+        topicPage.getPostButton().click();
+        Assert.assertEquals(topicPage.getTopicSubject().getText(), topic_title);
 
-		assertContainsInString(topicPage.getTopicMessage().getText(), message);
-	}
+        assertContainsInString(topicPage.getTopicMessage().getText(), message);
+    }
 
 }
