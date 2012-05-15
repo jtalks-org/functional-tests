ae2c951932bd1320566477e224fdbbc82e8b2f4f
Erik Khalimov <biomaks@gmail.com>
2012-05-15 20:17:17 +0400

    #JC-453 functional test jc117 added

---
+++ functional-tests-jcommune/src/test/java/org/jtalks/tests/jcommune/tests/topic/JC117PaginationForRegisteredUser.java
@@ -0,0 +1,74 @@
+package org.jtalks.tests.jcommune.tests.topic;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.*;
+
+
+/**
+ * author: erik
+ */
+public class JC117PaginationForRegisteredUser {
+    String value;
+    String postLink;
+    Boolean wasValueChanged = false;
+
+    @BeforeMethod
+    @Parameters({"app-url", "uUsername", "uPassword"})
+    public void setUp(String appUrl, String username, String password) {
+        driver.get(appUrl);
+        signIn(username, password);
+        mainPage.getProfileLink().click();
+
+        //here we will change count of posts for user in profile if it not equals 5
+        if (!driver.findElement(By.xpath("//li[@class='forum_row' and label='Page size']/span")).getText().equals("5")) {
+            wasValueChanged = true;
+            profilePage.getEditProfileButton().click();
+            value = driver.findElement(By.xpath("//li[@class='forum_row' and label= 'Page size']//select")).getAttribute("value");
+            Select select = new Select(driver.findElement(By.xpath("//li[@class='forum_row' and label= 'Page size']//select")));
+            select.selectByValue("5");
+            profilePage.getSaveEditButton().click();
+        }
+
+        driver.get(appUrl);
+        clickOnRandomSection();
+        clickOnRandomBranch();
+        createTopicForTest();
+        postLink = driver.getCurrentUrl();
+        createPostsForTest(5, 50);
+    }
+
+    @AfterMethod
+    @Parameters({"app-url", "uUsername"})
+    public void back(String appUrl, String username) {
+
+        //here we will change count of posts for user in profile how it was before test
+        if (wasValueChanged) {
+            driver.get(appUrl);
+            mainPage.getProfileLink().click();
+            profilePage.getEditProfileButton().click();
+            Select select = new Select(driver.findElement(By.xpath("//li[@class='forum_row' and label= 'Page size']//select")));
+            select.selectByValue(value);
+            profilePage.getSaveEditButton().click();
+            System.out.println(postLink);
+        }
+    }
+
+    @Test
+    public void testPagination() {
+        driver.get(postLink);
+        if (postPage.getPostsMessages().size() != 5) {
+            Assert.fail("Post page contains count of posts that not equals 5");
+        }
+        driver.findElement(By.xpath("//a[@class='page']")).click();
+        if (postPage.getPostsMessages().size() != 1) {
+            Assert.fail("Post page contains count of posts that not equals 1");
+        }
+    }
+}
