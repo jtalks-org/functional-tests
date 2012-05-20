5c794374f1f4f8b1848392c05835853a99d0cc87
masyan <jmasyan@gmail.com>
2012-05-20 18:12:27 +0600

    fix

--- functional-tests-jcommune/src/test/java/org/jtalks/tests/jcommune/tests/topic/JC117PaginationForRegisteredUser.java
+++ functional-tests-jcommune/src/test/java/org/jtalks/tests/jcommune/tests/topic/JC117PaginationForRegisteredUser.java
@@ -8,67 +8,77 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.*;
+import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.clickOnRandomBranch;
+import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.clickOnRandomSection;
+import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.createPostsForTest;
+import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.createTopicForTest;
+import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.driver;
+import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.logOut;
+import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.mainPage;
+import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.postPage;
+import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.profilePage;
+import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.signIn;
 
 
 /**
  * author: erik
  */
 public class JC117PaginationForRegisteredUser {
-    String value;
-    String postLink;
-    Boolean wasValueChanged = false;
+	String value;
+	String postLink;
+	Boolean wasValueChanged = false;
 
-    @BeforeMethod
-    @Parameters({"app-url", "uUsername", "uPassword"})
-    public void setUp(String appUrl, String username, String password) {
-        driver.get(appUrl);
-        signIn(username, password);
-        mainPage.getProfileLink().click();
+	@BeforeMethod
+	@Parameters({"app-url", "uUsername", "uPassword"})
+	public void setUp(String appUrl, String username, String password) {
+		driver.get(appUrl);
+		signIn(username, password);
+		mainPage.getProfileLink().click();
 
-        //here we will change count of posts for user in profile if it not equals 5
-        if (!driver.findElement(By.xpath("//li[@class='forum_row' and label='Page size']/span")).getText().equals("5")) {
-            wasValueChanged = true;
-            profilePage.getEditProfileButton().click();
-            value = driver.findElement(By.xpath("//li[@class='forum_row' and label= 'Page size']//select")).getAttribute("value");
-            Select select = new Select(driver.findElement(By.xpath("//li[@class='forum_row' and label= 'Page size']//select")));
-            select.selectByValue("5");
-            profilePage.getSaveEditButton().click();
-        }
+		//here we will change count of posts for user in profile if it not equals 5
+		if (!driver.findElement(By.xpath("//li[@class='forum_row' and label='Page size']/span")).getText().equals("5")) {
+			wasValueChanged = true;
+			profilePage.getEditProfileButton().click();
+			value = driver.findElement(By.xpath("//li[@class='forum_row' and label= 'Page size']//select")).getAttribute("value");
+			Select select = new Select(driver.findElement(By.xpath("//li[@class='forum_row' and label= 'Page size']//select")));
+			select.selectByValue("5");
+			profilePage.getSaveEditButton().click();
+		}
 
-        driver.get(appUrl);
-        clickOnRandomSection();
-        clickOnRandomBranch();
-        createTopicForTest();
-        postLink = driver.getCurrentUrl();
-        createPostsForTest(5, 50);
-    }
+		driver.get(appUrl);
+		clickOnRandomSection();
+		clickOnRandomBranch();
+		createTopicForTest();
+		postLink = driver.getCurrentUrl();
+		createPostsForTest(5, 50);
+	}
 
-    @AfterMethod
-    @Parameters({"app-url", "uUsername"})
-    public void back(String appUrl, String username) {
+	@AfterMethod
+	@Parameters({"app-url", "uUsername"})
+	public void back(String appUrl, String username) {
 
-        //here we will change count of posts for user in profile how it was before test
-        if (wasValueChanged) {
-            driver.get(appUrl);
-            mainPage.getProfileLink().click();
-            profilePage.getEditProfileButton().click();
-            Select select = new Select(driver.findElement(By.xpath("//li[@class='forum_row' and label= 'Page size']//select")));
-            select.selectByValue(value);
-            profilePage.getSaveEditButton().click();
-            System.out.println(postLink);
-        }
-    }
+		//here we will change count of posts for user in profile how it was before test
+		if (wasValueChanged) {
+			driver.get(appUrl);
+			mainPage.getProfileLink().click();
+			profilePage.getEditProfileButton().click();
+			Select select = new Select(driver.findElement(By.xpath("//li[@class='forum_row' and label= 'Page size']//select")));
+			select.selectByValue(value);
+			profilePage.getSaveEditButton().click();
+		}
 
-    @Test
-    public void testPagination() {
-        driver.get(postLink);
-        if (postPage.getPostsMessages().size() != 5) {
-            Assert.fail("Post page contains count of posts that not equals 5");
-        }
-        driver.findElement(By.xpath("//a[@class='page']")).click();
-        if (postPage.getPostsMessages().size() != 1) {
-            Assert.fail("Post page contains count of posts that not equals 1");
-        }
-    }
+		logOut(appUrl);
+	}
+
+	@Test
+	public void testPagination() {
+		driver.get(postLink);
+		if (postPage.getPostsMessages().size() != 5) {
+			Assert.fail("Post page contains count of posts that not equals 5");
+		}
+		driver.findElement(By.xpath("//a[@class='page']")).click();
+		if (postPage.getPostsMessages().size() != 1) {
+			Assert.fail("Post page contains count of posts that not equals 1");
+		}
+	}
 }
