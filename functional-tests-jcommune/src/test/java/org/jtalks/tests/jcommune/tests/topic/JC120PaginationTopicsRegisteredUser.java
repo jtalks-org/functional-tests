8acde310a183ab95de5c34fc73185f70eeb8023f
Erik Khalimov <biomaks@gmail.com>
2012-08-19 10:41:36 +0400

    JC120  fix

--- functional-tests-jcommune/src/test/java/org/jtalks/tests/jcommune/tests/topic/JC120PaginationTopicsRegisteredUser.java
+++ functional-tests-jcommune/src/test/java/org/jtalks/tests/jcommune/tests/topic/JC120PaginationTopicsRegisteredUser.java
@@ -1,14 +1,9 @@
 package org.jtalks.tests.jcommune.tests.topic;
 
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.SkipException;
 import org.testng.annotations.*;
 
-import java.util.List;
-
 import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.*;
-import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.profilePage;
 
 
 /**
@@ -26,20 +21,6 @@ public class JC120PaginationTopicsRegisteredUser {
 
         branchLink = driver.getCurrentUrl();
 
-
-        //set the page size 5  and check
-        profilePage.getCurrentUserLink().click();
-        if (!profilePage.getPageSizeTableField().getText().equals(Integer.toString(pageSizeDefault))){
-            profilePage.getEditProfileButton().click();
-            profilePage.selectPageSizeByValue(pageSizeDefault);
-            profilePage.getSaveEditButton().click();
-            if (!profilePage.getPageSizeTableField().getText().equals(Integer.toString(pageSizeDefault))){
-                throw new SkipException("Unable to set page size. Page size for user is " +
-                        profilePage.getPageSizeTableField().getText());
-            }
-
-        }
-
         driver.get(branchLink);
     }
 
@@ -48,8 +29,9 @@ public class JC120PaginationTopicsRegisteredUser {
     public void logout(String appUrl) {
         logOut(appUrl);
     }
+
     @BeforeMethod
-    public void navigateToTopicsPage(){
+    public void navigateToTopicsPage() {
         driver.get(branchLink);
     }
 
@@ -60,12 +42,12 @@ public class JC120PaginationTopicsRegisteredUser {
     }
 
     @Test
-    public void paginationButtonsShouldBePresentOnFirstPage(){
+    public void paginationButtonsShouldBePresentOnFirstPage() {
         Assert.assertNotEquals(topicPage.getTopicsButtons().size(), 0);
     }
 
     @Test
-    public void paginationButtonsShouldBePresentOnSecondPage(){
+    public void paginationButtonsShouldBePresentOnSecondPage() {
         topicPage.getButtonTopicsPageLink(2).click();
         Assert.assertNotEquals(topicPage.getTopicsButtons().size(), 0);
     }
