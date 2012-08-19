fa6479be3a4d62faf265dc9d200d4cc2f8e1d3d9
Erik Khalimov <biomaks@gmail.com>
2012-08-19 10:39:53 +0400

    JC119  fix

--- functional-tests-jcommune/src/test/java/org/jtalks/tests/jcommune/tests/topic/JC119PaginationForUnregisteredUser.java
+++ functional-tests-jcommune/src/test/java/org/jtalks/tests/jcommune/tests/topic/JC119PaginationForUnregisteredUser.java
@@ -1,6 +1,5 @@
 package org.jtalks.tests.jcommune.tests.topic;
 
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
@@ -14,18 +13,17 @@ import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.*;
 public class JC119PaginationForUnregisteredUser {
     @BeforeMethod
     @Parameters({"app-url"})
-
     public void setUp(String appUrl) {
         driver.get(appUrl);
         clickOnRandomBranch();
     }
 
     @Test
-    @Parameters ("pageSizeForUnregisteredUser")
+    @Parameters("pageSizeForUnregisteredUser")
     public void topicsCountShouldBeFiftyOnFirstPage(int pageSize) {
         int topicsSize = topicPage.getTopicsList().size();
         Assert.assertEquals(topicsSize, pageSize,
                 "\nSelected branch: " + branchPage.getBranchName()
-                + "\nTopics: " + topicsSize);
+                        + "\nTopics: " + topicsSize);
     }
 }
