b32948edc63fe8f750b01bc29d499e5df1eee9d1
Erik Khalimov <biomaks@gmail.com>
2012-09-15 20:34:16 +0400

    jc41, jc61 are desabled because of JC-967

--- functional-tests-jcommune/src/test/java/org/jtalks/tests/jcommune/tests/topic/JC41TopicEditWithoutSaveChanges.java
+++ functional-tests-jcommune/src/test/java/org/jtalks/tests/jcommune/tests/topic/JC41TopicEditWithoutSaveChanges.java
@@ -7,13 +7,7 @@ import org.testng.annotations.Test;
 import utils.StringHelp;
 
 import static org.jtalks.tests.jcommune.assertion.Existance.assertionNotContainsInString;
-import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.clickOnRandomBranch;
-import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.createTopicForTest;
-import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.driver;
-import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.logOut;
-import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.postPage;
-import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.signIn;
-import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.topicPage;
+import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.*;
 import static org.testng.Assert.assertNotEquals;
 
 /**
@@ -21,36 +15,35 @@ import static org.testng.Assert.assertNotEquals;
  */
 public class JC41TopicEditWithoutSaveChanges {
 
-	@BeforeMethod(alwaysRun = true)
-	@Parameters({"app-url", "uUsername", "uPassword"})
-	public void setupCase(String appUrl, String username, String password) {
-		driver.get(appUrl);
-		signIn(username, password);
-		clickOnRandomBranch();
-		createTopicForTest();
-		postPage.getEditTopicButton().click();
-	}
+    @BeforeMethod(alwaysRun = true, enabled = false)
+    @Parameters({"app-url", "uUsername", "uPassword"})
+    public void setupCase(String appUrl, String username, String password) {
+        driver.get(appUrl);
+        signIn(username, password);
+        clickOnRandomBranch();
+        createTopicForTest();
+        postPage.getEditTopicButton().click();
+    }
 
-	@AfterMethod(alwaysRun = true)
-	@Parameters({"app-url"})
-	public void destroy(String appUrl) {
-		logOut(appUrl);
+    @AfterMethod(alwaysRun = true, enabled = false)
+    @Parameters({"app-url"})
+    public void destroy(String appUrl) {
+        logOut(appUrl);
 
-	}
+    }
 
-	@Test
-	public void editTopicWithoutSaveChangesTest() {
+    @Test(enabled = false)
+    public void editTopicWithoutSaveChangesTest() {
 
-		String validTitle = StringHelp.getRandomString(10);
-		String validMessage = StringHelp.getRandomString(10);
-		topicPage.getSubjectField().clear();
-		topicPage.getMessageField().clear();
-		topicPage.getSubjectField().sendKeys(validTitle);
-		topicPage.getMessageField().sendKeys(validMessage);
-		topicPage.getBackButtonOnEditForm().click();
+        String validTitle = StringHelp.getRandomString(10);
+        String validMessage = StringHelp.getRandomString(10);
+        topicPage.getSubjectField().clear();
+        topicPage.getMessageField().clear();
+        topicPage.getSubjectField().sendKeys(validTitle);
+        topicPage.getMessageField().sendKeys(validMessage);
+        topicPage.getBackButtonOnEditForm().click();
 
-		assertNotEquals(topicPage.getTopicSubject().getText(), validTitle);
-
-		assertionNotContainsInString(topicPage.getTopicMessage().getText(), validMessage);
-	}
+        assertNotEquals(topicPage.getTopicSubject().getText(), validTitle);
+        assertionNotContainsInString(topicPage.getTopicMessage().getText(), validMessage);
+    }
 }
--- functional-tests-jcommune/src/test/java/org/jtalks/tests/jcommune/tests/topic/JC61TopicPositionWithoutSaveChanges.java
+++ functional-tests-jcommune/src/test/java/org/jtalks/tests/jcommune/tests/topic/JC61TopicPositionWithoutSaveChanges.java
@@ -7,58 +7,51 @@ import org.testng.annotations.Test;
 import utils.CollectionHelp;
 import utils.StringHelp;
 
-import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.branchPage;
-import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.clickOnRandomBranch;
-import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.createTopicForTest;
-import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.driver;
-import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.logOut;
-import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.postPage;
-import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.signIn;
-import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.topicPage;
+import static org.jtalks.tests.jcommune.common.JCommuneSeleniumTest.*;
 import static org.testng.Assert.assertNotEquals;
 
 /**
  * @author masyan
  */
 public class JC61TopicPositionWithoutSaveChanges {
-	String editedSubject;
+    String editedSubject;
 
-	@BeforeMethod(alwaysRun = true)
-	@Parameters({"app-url", "uUsername", "uPassword"})
-	public void setupCase(String appUrl, String username, String password) {
-		driver.get(appUrl);
-		signIn(username, password);
-		clickOnRandomBranch();
-		//create 2 topics
-		createTopicForTest();
-		branchPage.getBreadCrumbsBranchLink().click();
-		createTopicForTest();
-		branchPage.getBreadCrumbsBranchLink().click();
+    @BeforeMethod(alwaysRun = true, enabled = false)
+    @Parameters({"app-url", "uUsername", "uPassword"})
+    public void setupCase(String appUrl, String username, String password) {
+        driver.get(appUrl);
+        signIn(username, password);
+        clickOnRandomBranch();
+        //create 2 topics
+        createTopicForTest();
+        branchPage.getBreadCrumbsBranchLink().click();
+        createTopicForTest();
+        branchPage.getBreadCrumbsBranchLink().click();
 
-		CollectionHelp.getWebElementFromCollectionByIndex(topicPage.getTopicsList(), 2).click();
+        CollectionHelp.getWebElementFromCollectionByIndex(topicPage.getTopicsList(), 2).click();
 
-	}
+    }
 
-	@AfterMethod(alwaysRun = true)
-	@Parameters({"app-url"})
-	public void destroy(String appUrl) {
-		logOut(appUrl);
+    @AfterMethod(alwaysRun = true, enabled = false)
+    @Parameters({"app-url"})
+    public void destroy(String appUrl) {
+        logOut(appUrl);
 
-	}
+    }
 
-	@Test
-	public void topicPositionWithoutSaveChangesTest() {
-		//first step
-		editedSubject = topicPage.getTopicSubject().getText();
-		postPage.getEditTopicButton().click();
-		topicPage.getMessageField().clear();
-		topicPage.getMessageField().sendKeys(StringHelp.getRandomString(10));
-		topicPage.getBackButtonOnEditForm().click();
-		branchPage.getBreadCrumbsBranchLink().click();
+    @Test(enabled = false)
+    public void topicPositionWithoutSaveChangesTest() {
+        //first step
+        editedSubject = topicPage.getTopicSubject().getText();
+        postPage.getEditTopicButton().click();
+        topicPage.getMessageField().clear();
+        topicPage.getMessageField().sendKeys(StringHelp.getRandomString(10));
+        topicPage.getBackButtonOnEditForm().click();
+        branchPage.getBreadCrumbsBranchLink().click();
 
-		//second step
-		CollectionHelp.getFirstWebElementFromCollection(topicPage.getTopicsList()).click();
-		assertNotEquals(topicPage.getTopicSubject().getText(), editedSubject);
+        //second step
+        CollectionHelp.getFirstWebElementFromCollection(topicPage.getTopicsList()).click();
+        assertNotEquals(topicPage.getTopicSubject().getText(), editedSubject);
 
-	}
+    }
 }
