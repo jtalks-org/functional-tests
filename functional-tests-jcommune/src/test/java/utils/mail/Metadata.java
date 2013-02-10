7e986f060184ebca208d5def6a11d5154266cc8e
Guram Savinov <guram.savinov@gmail.com>
2013-02-11 03:13:03 +0400

    #JC-1241 Implemented Mailtrap messages metadata parser. Added getting activation link for user operation.

---
+++ functional-tests-jcommune/src/test/java/utils/mail/Metadata.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (C) 2011  JTalks.org Team
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package utils.mail;
+
+import org.joda.time.DateTime;
+
+/**
+ * @author Guram Savinov
+ */
+public class Metadata {
+
+    private String id;
+    private String title;
+    private DateTime dateTime;
+    private String recipient;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public DateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(DateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+}
