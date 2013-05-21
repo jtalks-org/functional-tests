13b4e814af3155409ab3ecc94885df5a996190c3
ctapobep <ctapobep@javatalks.ru>
2013-05-22 02:09:19 +0400

    Removed extra logging

--- jcommune-webdriver/src/main/java/org/jtalks/tests/jcommune/webdriver/JCommuneSeleniumTest.java
+++ jcommune-webdriver/src/main/java/org/jtalks/tests/jcommune/webdriver/JCommuneSeleniumTest.java
@@ -6,15 +6,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class JCommuneSeleniumTest {
@@ -22,14 +19,13 @@ public class JCommuneSeleniumTest {
     public static final String JCOMMUNE_CONTEXT_PATH = "/jcommune";
     private static final int SELENIUM_TIMEOUT = 10;
     public static String webdriverType;
-    private Logger logger = LoggerFactory.getLogger(getClass());
 
     /**
      * Method  execute before execute Test. This method getting  driver for connect Remote Selenium Server. All values
      * are stored in testng.xml file.
      *
-     * @param webDriverUrl  selenium server URL, will be used by default if no SELENIUM_URL env var is set (like in
-     *                      case with SauceLabs integration on Jenkins)
+     * @param webDriverUrl selenium server URL, will be used by default if no SELENIUM_URL env var is set (like in case
+     *                     with SauceLabs integration on Jenkins)
      */
     @BeforeSuite(alwaysRun = true)
     @Parameters({"webDriverUrl", "appUrl"})
@@ -46,10 +42,6 @@ public class JCommuneSeleniumTest {
         capabilities.setVersion(getBrowserVersion());
 
         String seleniumUrl = getSeleniumUrl(defaultSeleniumServerUrl);
-        logger.info("Selenium URL: [{}]", seleniumUrl);
-        for (Map.Entry<String, String> var : System.getenv().entrySet()) {
-            logger.info("{}={}", var.getKey(), var.getValue());
-        }
         driver = new RemoteWebDriver(new URL(seleniumUrl), capabilities);
         driver.manage().timeouts().implicitlyWait(SELENIUM_TIMEOUT, TimeUnit.SECONDS);
     }
