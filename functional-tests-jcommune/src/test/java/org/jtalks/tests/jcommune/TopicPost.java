ceb6220486e8494397801d7bf5330c0f1c0078d4
Guram Savinov <guram.savinov@gmail.com>
2013-05-24 20:19:43 +0400

    Added method to create topic with given title and message. Added separated test-suite for the tests under development.

---
+++ functional-tests-jcommune/src/test/java/org/jtalks/tests/jcommune/TopicPost.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (C) 2011  JTalks.org Team
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package org.jtalks.tests.jcommune;
+
+import org.jtalks.tests.jcommune.webdriver.User;
+import org.jtalks.tests.jcommune.webdriver.Users;
+import org.jtalks.tests.jcommune.webdriver.topic.Topics;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import static org.jtalks.tests.jcommune.webdriver.JCommuneSeleniumConfig.driver;
+import static org.jtalks.tests.jcommune.webdriver.page.Pages.mainPage;
+
+/**
+ * @author Guram Savinov
+ */
+public class TopicPost {
+
+    @BeforeMethod
+    @Parameters({"appUrl"})
+    public void setupCase(String appUrl) {
+		driver.get(appUrl);
+        mainPage.logOutIfLoggedIn();
+    }
+
+    @Test(groups = { "develop" })
+    public void createTopic() throws Exception {
+        User user = Users.signUp();
+        Users.signIn(user);
+        Topics.createTopic("subject", "message");
+    }
+}
--- jcommune-webdriver/src/main/java/org/jtalks/tests/jcommune/webdriver/topic/Topics.java
+++ jcommune-webdriver/src/main/java/org/jtalks/tests/jcommune/webdriver/topic/Topics.java
@@ -1,7 +1,41 @@
+/**
+ * Copyright (C) 2011  JTalks.org Team
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 package org.jtalks.tests.jcommune.webdriver.topic;
+
+import org.jtalks.tests.jcommune.mail.mailtrap.exceptions.CouldNotGetMessageException;
+import org.jtalks.tests.jcommune.mail.mailtrap.exceptions.CouldNotGetMessagesException;
+import org.jtalks.tests.jcommune.webdriver.exceptions.CouldNotSignInUserException;
+import org.jtalks.tests.jcommune.webdriver.exceptions.MailWasNotReceivedException;
+import org.jtalks.tests.jcommune.webdriver.exceptions.ValidationException;
+
+import static org.jtalks.tests.jcommune.webdriver.page.Pages.branchPage;
+import static org.jtalks.tests.jcommune.webdriver.page.Pages.topicPage;
 
 /**
  * Contain topic actions like creating, deleting etc.
  */
 public class Topics {
+
+    public static void createTopic(String subject, String message) throws MailWasNotReceivedException,
+            CouldNotGetMessagesException, CouldNotGetMessageException, ValidationException,
+            CouldNotSignInUserException {
+        branchPage.getBranchList().get(0).click();
+        topicPage.getNewButton().click();
+        topicPage.getSubjectField().sendKeys(subject);
+        topicPage.getMessageField().sendKeys(message);
+        topicPage.getPostButton().click();
+    }
 }
