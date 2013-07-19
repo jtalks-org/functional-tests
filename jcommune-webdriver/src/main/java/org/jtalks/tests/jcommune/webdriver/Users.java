9217d4c53e93866e2ac35b2e08e014f2cc98ff6c
Guram Savinov <guram.savinov@gmail.com>
2013-07-20 01:58:34 +0400

    #JC-1159
    Restored sign-in operation checking by profile link existence.

--- jcommune-webdriver/src/main/java/org/jtalks/tests/jcommune/webdriver/Users.java
+++ jcommune-webdriver/src/main/java/org/jtalks/tests/jcommune/webdriver/Users.java
@@ -81,6 +81,9 @@ public class Users {
             }
             throw new ValidationException(failedFields);
         }
+        if (!mainPage.userIsLoggedIn()) {
+            throw new CouldNotOpenPageException("profile page for the user: " + user.getEmail());
+        }
     }
 
     /**
