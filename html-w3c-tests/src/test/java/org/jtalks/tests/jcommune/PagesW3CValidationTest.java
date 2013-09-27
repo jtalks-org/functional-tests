83174c17e46cc070b536603a316971605d75c32c
ctapobep <ctapobep@javatalks.ru>
2013-09-27 18:08:41 +0400

    Improved logging of w3c errors.

--- html-w3c-tests/src/test/java/org/jtalks/tests/jcommune/PagesW3CValidationTest.java
+++ html-w3c-tests/src/test/java/org/jtalks/tests/jcommune/PagesW3CValidationTest.java
@@ -5,15 +5,12 @@ import org.jtalks.tests.jcommune.webdriver.action.Topics;
 import org.jtalks.tests.jcommune.webdriver.action.Users;
 import org.jtalks.tests.jcommune.webdriver.entity.topic.Topic;
 import org.jtalks.tests.jcommune.webdriver.entity.user.User;
-import org.jtalks.tests.jcommune.webdriver.exceptions.PermissionsDeniedException;
-import org.jtalks.tests.jcommune.webdriver.exceptions.ValidationException;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import static org.jtalks.tests.jcommune.PageHtmlValidator.validatePage;
 import static org.jtalks.tests.jcommune.webdriver.JCommuneSeleniumConfig.driver;
-import static org.jtalks.tests.jcommune.webdriver.page.Pages.branchPage;
 import static org.jtalks.tests.jcommune.webdriver.page.Pages.mainPage;
 import static org.jtalks.tests.jcommune.webdriver.page.Pages.topicPage;
 
@@ -21,6 +18,11 @@ import static org.jtalks.tests.jcommune.webdriver.page.Pages.topicPage;
  * @author Leonid Kazancev
  */
 public class PagesW3CValidationTest {
+    private static String getPageSource() throws InterruptedException {
+        Thread.sleep(5000);
+        return driver.getPageSource();
+    }
+
     @BeforeMethod
     @Parameters("appUrl")
     public void setup(String appUrl) {
@@ -29,48 +31,48 @@ public class PagesW3CValidationTest {
     }
 
     @Test
-    public void mainPage_Test() throws ValidationException, InterruptedException {
+    public void mainPage_Test() throws Exception {
         String pageSource = getPageSource();
         validatePage(pageSource);
     }
 
     @Test
-    public void signUpPage_Test() throws ValidationException, InterruptedException {
+    public void signUpPage_Test() throws Exception {
         mainPage.clickRegistration();
         String pageSource = getPageSource();
         validatePage(pageSource);
     }
 
     @Test
-    public void signInPage_Test() throws ValidationException, InterruptedException {
+    public void signInPage_Test() throws Exception {
         mainPage.clickLogin();
         String pageSource = getPageSource();
         validatePage(pageSource);
     }
 
     @Test
-    public void topicPage_Test() throws ValidationException, PermissionsDeniedException, InterruptedException {
+    public void topicPage_Test() throws Exception {
         topicPage.goToTopicPage();
         String pageSource = getPageSource();
         validatePage(pageSource);
     }
 
     @Test
-    public void topicCreatePage_Test() throws ValidationException, PermissionsDeniedException, InterruptedException {
+    public void topicCreatePage_Test() throws Exception {
         topicPage.goToTopicCreatePage();
         String pageSource = getPageSource();
         validatePage(pageSource);
     }
 
     @Test
-    public void codeReviewCreatePage_Test() throws ValidationException, PermissionsDeniedException, InterruptedException {
+    public void codeReviewCreatePage_Test() throws Exception {
         topicPage.goToReviewCreatePage();
         String pageSource = getPageSource();
         validatePage(pageSource);
     }
 
     @Test
-    public void postPage_Test() throws ValidationException, PermissionsDeniedException, InterruptedException {
+    public void postPage_Test() throws Exception {
         Topic topic = new Topic(StringHelp.randomString(40), StringHelp.randomString(100));
         String branchTitle = "TestBranch";
         User user = Users.signUp();
@@ -80,10 +82,5 @@ public class PagesW3CValidationTest {
         Topics.postAnswer(topic, branchTitle);
         String pageSource = getPageSource();
         validatePage(pageSource);
-    }
-
-    private static String getPageSource() throws InterruptedException {
-        Thread.sleep(5000);
-        return driver.getPageSource();
     }
 }
