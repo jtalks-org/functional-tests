331f0491d7fc6c02fa8cf7a3b701b4482114d2dc
ctapobep <ctapobep@javatalks.ru>
2013-09-29 02:17:17 +0400

    Fixing formatting

--- jcommune-webdriver/src/main/java/org/jtalks/tests/jcommune/webdriver/page/ExternalLinksPage.java
+++ jcommune-webdriver/src/main/java/org/jtalks/tests/jcommune/webdriver/page/ExternalLinksPage.java
@@ -1,6 +1,5 @@
 package org.jtalks.tests.jcommune.webdriver.page;
 
-import org.jtalks.tests.jcommune.webdriver.JCommuneSeleniumConfig;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -10,56 +9,42 @@ import java.util.List;
 
 public class ExternalLinksPage {
     public static final String externalLinksSel = "//span[@id='externalLinks']/span/a";
-
     public static final String externalLinksFromDialogSel = "//div[@class='modal-body']/table/tbody/tr";
-
     public static final String externalLinksHrefFromDialogSel = "td[@class='link-url']";
-
     //class name
     public static final String externalLinksRemoveIconFromDialogSel = "icon-trash";
-
     @FindBy(id = "links_editor")
     private WebElement linksEditorBut;
-
     @FindBy(id = "addMainLink")
     private WebElement addLinkBut;
-
     @FindBy(id = "linkTitle")
     private WebElement titleField;
-
     @FindBy(id = "linkUrl")
     private WebElement urlField;
-
     @FindBy(id = "linkHint")
     private WebElement hintField;
-
     @FindBy(id = "saveLink")
     private WebElement saveLinkBut;
-
     @FindBy(id = "cancelLink")
     private WebElement cancelLinkBut;
-
     @FindBy(id = "removeLink")
     private WebElement removeLinkBut;
-
     @FindBy(xpath = externalLinksSel)
     private List<WebElement> externalLinks;
-
     @FindBy(className = "close")
     private WebElement closeDialogButton;
-
     @FindBy(xpath = externalLinksFromDialogSel)
     private List<WebElement> externalLinksFromDialog;
 
     public ExternalLinksPage(WebDriver driver) {
-		PageFactory.initElements(driver, this);
-	}
+        PageFactory.initElements(driver, this);
+    }
 
     public WebElement getLinksEditorBut() {
         return linksEditorBut;
     }
 
-    public WebElement getCloseDialogButton(){
+    public WebElement getCloseDialogButton() {
         return closeDialogButton;
     }
 
