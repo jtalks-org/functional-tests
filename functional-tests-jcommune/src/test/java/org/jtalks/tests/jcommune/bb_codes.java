592fc1f5e4dce868be6c614b88693b4861cd04da
pavelgradobik <gradobik@gmail.com>
2014-02-01 00:14:54 +0200

    bb-codes via array

--- functional-tests-jcommune/src/test/java/org/jtalks/tests/jcommune/bb_codes.java
+++ functional-tests-jcommune/src/test/java/org/jtalks/tests/jcommune/bb_codes.java
@@ -17,8 +17,6 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-import java.util.Date;
-
 import static org.apache.commons.lang.RandomStringUtils.randomAlphanumeric;
 import static org.jtalks.tests.jcommune.webdriver.JCommuneSeleniumConfig.driver;
 import static org.jtalks.tests.jcommune.webdriver.page.Pages.mainPage;
@@ -31,28 +29,39 @@ class bb_codes {
     // set a bb-code method
     public  setBBCode() {
         //set a string type for a future bb-codes, possible could be an array like {"b","/b"}
-        String bb_code() = {
-            return bb_code;
+        //String bb_code() = {
+        //    return bb_code;
+        //};
+        String[][] bb_code() = {
+            {"b","/b"}, {"i","/i"}
+            retunt bb_code; 
+            /*возможно бб коды описать массивами и вызывать по типу bb_code[0][1] который и будет
+            описывать открывающий или закрывающий тэг*/
         };
-        /* String[][] bb_code() = {
-            retunt bb_code //возможно бб коды описать массивами и вызывать по типу bb_code[0][1] который и будет
-            описывать открывающий или закрывающий тэг
-        }
-         */
+         
     }
+    
+    /*method for text comparison
+    public setText(String[], args) {
+        JTextPane textPane = new JTextPane();
+        textPane.setText("message"); // add text "message"
+        SimpleAttributeSet sas = new SimpleAttributeSet();
+        StyleConstants.setBold(sas, true);//text Bold 
+        textPane.
+        
+        } */
+    
+    
+    
 
-    SimpleAttributeSet attributes = new SimpleAttributeSet();
-    attributes.addAttribute(StyleConstants.CharacterConstants.Bold, Boolean.TRUE);
-    }
-
-    @Test
-    public void usingAbb_codeForASsingleExpressionInNewTopiсScreen_QA_7() throws Exception {
+    @Test(enabled = true)
+    public void usingAbb_codeForASsingleExpressionInNewTopiсScreen_QA_7() {
         Users.signUpAndSignIn();
-        setBBCode bb_code1 = new bb_codes();
+        /*setBBCode bb_code1 = new bb_codes();
         setBBCode bb_code2 = new bb_codes();
         bb_code1.setBBCode = "b";
-        bb_code2.setBBCode = "/b";
-        Topic topic = new Topic("subject", set_text_in);
+        bb_code2.setBBCode = "/b";*/
+        Topic topic = new Topic("subject", bb_code[0][0] + "message" + bb_code[0][1]);
         Topic createdTopic = Topics.createTopic(topic);
     }
 }
