936f404b21aa5b70d7d181c8a52c749a529e4e06
dantalia <qa.bogdanov@gmail.com>
2014-02-07 16:00:06 +0400

    JC-1747 Update business case for Profile

--- functional-tests-jcommune/src/test/java/org/jtalks/tests/jcommune/TestProfile.java
+++ functional-tests-jcommune/src/test/java/org/jtalks/tests/jcommune/TestProfile.java
@@ -1,6 +1,11 @@
 package org.jtalks.tests.jcommune;
 
+import org.apache.commons.lang3.RandomStringUtils;
+import org.jtalks.tests.jcommune.webdriver.action.Branches;
+import org.jtalks.tests.jcommune.webdriver.action.Topics;
 import org.jtalks.tests.jcommune.webdriver.action.Users;
+import org.jtalks.tests.jcommune.webdriver.entity.topic.Post;
+import org.jtalks.tests.jcommune.webdriver.entity.topic.Topic;
 import org.jtalks.tests.jcommune.webdriver.entity.user.User;
 import org.jtalks.tests.jcommune.webdriver.exceptions.ValidationException;
 import org.testng.annotations.BeforeMethod;
@@ -11,13 +16,9 @@ import static org.jtalks.tests.jcommune.webdriver.JCommuneSeleniumConfig.driver;
 import static org.jtalks.tests.jcommune.webdriver.page.Pages.mainPage;
 
 /**
- * Created with IntelliJ IDEA.
- * User: Zalk
- * Date: 06.02.14
- * Time: 21:41
- * To change this template use File | Settings | File Templates.
+ * @author Igor Bogdanov
  */
-public class TestProfile {
+public class ProfileTest {
 
     @BeforeMethod
     @Parameters({"appUrl"})
@@ -27,83 +28,83 @@ public class TestProfile {
     }
 
     @Test
-    public void openAndViewUserProfile_ShouldPass() throws Exception {
+    public void viewUserProfile_ShouldPass() throws Exception {
         User user = Users.signUpAndSignIn();
-        Profile.goToViewProfile();
+        Users.viewProfile(user);
     }
 
     @Test
-    public void openAndEditUserProfile_ShouldPass() throws Exception {
+    public void editProfileWithNoChanges_ShouldPass() throws Exception {
         User user = Users.signUpAndSignIn();
-        Profile.goToEditProfile();
-        Profile.saveChanges();
+        Users.editProfile(user);
+        Users.assertProfileEquals(user);
     }
 
 
     @Test
-    public void enterValidFirstNameBetweenZeroAnd45Chars_ShouldPass() throws Exception {
+    public void firstNameBetweenZeroAnd45Chars_ShouldPass() throws Exception {
         User user = Users.signUpAndSignIn();
-        Profile.goToEditProfile();
+        Users.editProfile();
         Profile.enterValidFirstName();
         Profile.saveChanges();
     }
 
-    @Test
-    public void enterVeryLongFirstName_ShouldFail() throws Exception {
+    @Test(expectedExceptions = ValidationException.class)
+    public void veryLongFirstName_ShouldFail() throws Exception {
         User user = Users.signUpAndSignIn();
-        Profile.goToEditProfile();
+        Users.editProfile();
         Profile.enterVeryLongFirstName();
         Profile.saveChanges();
     }
 
     @Test
-    public void enterValidLastNameBetweenZeroAnd255Chars_ShouldPass() throws Exception {
+    public void lastNameBetweenZeroAnd255Chars_ShouldPass() throws Exception {
         User user = Users.signUpAndSignIn();
-        Profile.goToEditProfile();
+        Users.editProfile();
         Profile.enterValidLastName();
         Profile.saveChanges();
     }
 
     @Test
-    public void enterVeryLongLastName_ShouldFail() throws Exception {
+    public void veryLongLastName_ShouldFail() throws Exception {
         User user = Users.signUpAndSignIn();
-        Profile.goToEditProfile();
+        Users.editProfile();
         Profile.enterVeryLongLastName();
         Profile.saveChanges();
     }
 
     @Test
-    public void enterValidSignatureBetweenZeroAnd255Chars_ShouldPass() throws Exception {
+    public void signatureBetweenZeroAnd255Chars_ShouldPass() throws Exception {
         User user = Users.signUpAndSignIn();
-        Profile.goToEditProfile();
+        Users.editProfile();
         Profile.enterValidSignature();
         Profile.saveChanges();
     }
 
     @Test
-    public void enterVeryLongSignature_ShouldFail() throws Exception {
+    public void veryLongSignature_ShouldFail() throws Exception {
         User user = Users.signUpAndSignIn();
-        Profile.goToEditProfile();
+        Users.editProfile();
         Profile.enterVeryLongSignature();
         Profile.saveChanges();
     }
 
     @Test
-    public void enterValidEmail_ShouldPass() throws Exception {
+    public void validEmail_ShouldPass() throws Exception {
     }
 
     @Test
-    public void enterVeryLongEmail_ShouldFail() throws Exception {
+    public void veryLongEmail_ShouldFail() throws Exception {
         User user = Users.signUpAndSignIn();
-        Profile.goToEditProfile();
+        Users.editProfile();
         Profile.enterVeryLongEmail();
         Profile.saveChanges();
     }
 
     @Test
-    public void enterIncorrectEmailWithSpecialSymbols_ShouldFail throws Exception {
+    public void incorrectEmailWithSpecialSymbols_ShouldFail throws Exception {
         User user = Users.signUpAndSignIn();
-        Profile.goToEditProfile();
+        Users.editProfile();
         Profile.enterIncorrectEmailWithSpecailSymbols();
         Profile.saveChanges();
     }
@@ -111,8 +112,95 @@ public class TestProfile {
     @Test
     public void emptyFieldEmail_ShouldFail() throws Exception {
         User user = Users.signUpAndSignIn();
-        Profile.goToEditProfile();
+        Users.editProfile();
         Profile.deletDataInFieldEmail();
+        Profile.saveChanges();
+    }
+
+    @Test
+    public void changePageSizeAndCheckMessageInTheTopic() throws Exception {
+        User user = Users.signUpAndSignIn();
+        Topic newTopic = Topics.createTopic(new Topic(new Post, "text1"));
+        for (int i = 0; i < 3; i++) {
+            Users.editProfile();
+            int postCount = Profile.changePageSize();
+            Profile.saveChanges();
+            Branches.openBranch(newTopic.getBranch().getTitle());
+            for (int j = postCount; j > 0; j++) {
+                int count = Profile.checkPostInTopic(j);
+                if (count == postCount) {
+                    continue;
+                }
+                else {
+                    System.out.println("Page size was choosen " + postCount + ",but in page was found " + count);
+                }
+            }
+        }
+    }
+
+    @Test
+    public void enterValidDataInFieldLocation_ShouldPass() throws Exception {
+        User user = Users.signUpAndSignIn();
+        Users.editProfile();
+        Profile.enterValidDataInFieldLocation();
+        Profile.saveChanges();
+    }
+
+    @Test
+    public void enterVeryLongLocation_ShouldFail() throws Exception {
+        User user = Users.signUpAndSignIn();
+        Users.editProfile();
+        Profile.enterVeryLongLocation();
+        Profile.saveChanges();
+    }
+
+    @Test
+    public void enterNewPasswordWithoutConfirmNewPasswordAndCurrentPassword_ShouldFail() throws Exception {
+        User user = Users.signUpAndSignIn();
+        Users.editProfile();
+        Profile.enterNewPassword();
+        Profile.saveChanges();
+    }
+
+    @Test
+    public void enterNewPasswordAndConfirmNewPasswordWithoutCurrentPassword_ShouldFail() throws Exception {
+        User user = Users.signUpAndSignIn();
+        Users.editProfile();
+        Profile.enterNewPassword();
+        Profile.enterConfirmPassword();
+        Profile.saveChanges();
+    }
+
+    @Test
+    public void enterNewPasswordAndConfirmNewPasswordAndCurrentPassword_ShouldPass()	throws Exception {
+        User user = Users.signUpAndSignIn();
+        String newPass = RandomStringUtils.randomAlphanumeric(25);
+        Users.editProfile();
+        Profile.enterNewPassword(newPass);
+        Profile.enterConfirmPassword(newPass);
+        Profile.enterCurrentPassword(user);
+        Profile.saveChanges();
+    }
+
+    @Test
+    public void enterNewPasswordAndCurrentPasswordAndIncorrectConfirmPassword_ShouldFail() throws Exception {
+        User user = Users.signUpAndSignIn();
+        String newPass = RandomStringUtils.randomAlphanumeric(25);
+        Users.editProfile();
+        Profile.enterNewPassword(newPass);
+        Profile.enterConfirmPassword(RandomStringUtils.randomAlphanumeric(25));
+        Profile.enterCurrentPassword(user);
+        Profile.saveChanges();
+    }
+
+    @Test
+    public void enterNewPasswordAndConfirmPasswordAndIncorrectCurrentPassword_ShouldFail() throws Exception {
+        User user = Users.signUpAndSignIn();
+        String newPass = RandomStringUtils.randomAlphanumeric(25);
+        Users.editProfile();
+        Profile.enterNewPassword(newPass);
+        Profile.enterConfirmPassword(newPass);
+        Profile.enterIncorrectCurrentPassword();
         Profile.saveChanges();
     }
 }
