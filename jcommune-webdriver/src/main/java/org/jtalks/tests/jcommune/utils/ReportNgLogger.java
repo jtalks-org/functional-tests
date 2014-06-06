bb9add26d68d49109b1a151d2a841346af436440
Yuliya <xcandlelight@gmail.com>
2014-06-06 13:28:51 +0400

    allure reporting connection

--- jcommune-webdriver/src/main/java/org/jtalks/tests/jcommune/utils/ReportNgLogger.java
+++ jcommune-webdriver/src/main/java/org/jtalks/tests/jcommune/utils/ReportNgLogger.java
@@ -5,16 +5,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Reporter;
 import org.testng.annotations.*;
+import ru.yandex.qatools.allure.events.*;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
 
+import static ru.yandex.qatools.allure.Allure.LIFECYCLE;
+
 public class ReportNgLogger {
     private static final Logger LOGGER = LoggerFactory.getLogger(ReportNgLogger.class);
     public static void info(String toLog) {
         LOGGER.info(toLog);
+        LIFECYCLE.fire(new StepStartedEvent(toLog));
+        LIFECYCLE.fire(new StepFinishedEvent());
         String logPrefix = "|" + StringUtils.repeat("_", stackTraceDepth() - 3);//-3 because current methods also count
         Reporter.log(logPrefix + toLog);
     }
