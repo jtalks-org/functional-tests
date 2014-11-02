01086df46a88bf77e16bcebb5d26b575d48a9175
DentonA <DentonA@yandex.ru>
2014-11-02 14:53:16 +0200

    Added some Antarcticle groovy tests for translation

--- functional-tests-antarcticle/src/test/groovy/org/jtalks/tests/antarcticle/ArticleManagementTest.groovy
+++ functional-tests-antarcticle/src/test/groovy/org/jtalks/tests/antarcticle/ArticleManagementTest.groovy
@@ -41,4 +41,69 @@ class ArticleManagementTest extends Specification {
       'Max boundary'  | randomAlphabetic(65000)  | 'Could not create an article with max content boundary'
       'Average title' | randomAlphabetic(100)    | 'Could not create an article of average content length'
   }
-}  
+
+  def 'translation title positive cases'() {
+    given: 'user is created and logged in'
+      and: 'article is created and opened'
+    when: "user creates a translation with $caseName"
+    then: 'translation should get created'
+    where:
+      caseName        | articleTitle         | messageIfCaseFailed
+      'Min boundary'  | randomAlphabetic(1)  | 'Could not create translation of article with min title boundary'
+      'Max boundary'  | randomAlphabetic(60) | 'Could not create a translation of article with max title boundary'
+      'Average title' | randomAlphabetic(30) | 'Could not create a translation of article of average title length'
+  }
+
+  def 'translation title negative cases'() {
+    given: 'user is created and logged in'
+      and: 'article is created and opened'
+    when: "user creates a translation with $caseName"
+    then: 'translation should not get created'
+    where:
+      caseName          | articleTitle         | messageIfCaseFailed
+      'Empty title'     | ''                   | 'Translation of article with empty title was possible to create while this is not allowed'
+      'Spaces in title' | ' '                  | 'Title with only spaces should not be allowed!'
+      'Too long title'  | randomAlphabetic(61) | 'It was possible to create a translation of article with too long title'
+  }
+
+  def 'translation content positive cases'() {
+    given: 'user is created and logged in'
+      and: 'article is created and opened'
+    when: "user creates a translation with $caseName"
+    then: 'translation should get created'
+    where:
+      caseName        | articleContent          | messageIfCaseFailed
+      'Empty content' | ''                      | 'Could not create translation of article with empty body'
+      'Max boundary'  | randomAlphabetic(65000) | 'Could not create a translation of article with max content boundary'
+      'Average title' | randomAlphabetic(100)   | 'Could not create a translation of article of average content length'
+  }
+
+  def "translation content negative cases"() {
+    given: 'user is created and logged in'
+      and: 'article is created and opened'
+    when: "user creates a translation with $caseName"
+    then: 'translation of article should not get created'
+    where:
+      caseName          | articleContent          | messageIfCaseFailed
+      'Max boundary +1' | randomAlphabetic(65001) | 'It was possible to create a translation of article with too long content'
+  }
+
+  def "create translation as anonymous user (should fail)"() {
+    given: 'article is created and opened'
+    when: "user creates a translation"
+    then: 'translation of article should not get created'
+  }
+
+  def "create second translation"() {
+    given: 'user is created and logged in'
+      and: 'article is created and opened'
+      and: 'translation in English is created'
+    when: "user creates a translation on $language"
+    then: 'translation of article should get created'
+    where:
+      language        | translationLanguage | messageIfCaseFailed
+      'second'        | $randomLanguage     | 'Could not create second translation of article'
+      'same language' | 'English'           | 'Could not create translation of article on same language'
+  }
+
+}
