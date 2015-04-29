b4fe282a4587c7f0bb41983b4bac4d4c8181d9a8
timisoreana <nalavi124@yahoo.com>
2015-04-29 22:55:12 +0300

    MoveTopicPermissionsTest

---
+++ jcommune-webdriver/src/main/java/org/jtalks/tests/jcommune/webdriver/entity/topic/QA.java
@@ -0,0 +1,26 @@
+package org.jtalks.tests.jcommune.webdriver.entity.topic;
+
+
+import com.google.common.base.Splitter;
+import org.jtalks.tests.jcommune.webdriver.entity.branch.Branch;
+import org.apache.commons.lang3.StringUtils;
+import java.util.List;
+
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
+
+
+/**
+ * JCommune QA representation
+ */
+
+public class QA  extends Topic {
+    private String content = randomAlphanumeric(50);
+
+    public QA() {
+    }
+
+    public QA(String title, String content) {
+        this.withTitle(title);
+        this.content = content;
+    }
+}
