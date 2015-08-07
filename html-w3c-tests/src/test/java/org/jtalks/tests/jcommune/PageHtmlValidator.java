c35d22b62a3e309b3166915a3f9b647b21f4f55e
masyan <jmasyan@gmail.com>
2015-08-07 11:33:13 +0500

    add  missing dependencies

--- html-w3c-tests/src/test/java/org/jtalks/tests/jcommune/PageHtmlValidator.java
+++ html-w3c-tests/src/test/java/org/jtalks/tests/jcommune/PageHtmlValidator.java
@@ -7,6 +7,8 @@ import com.jcabi.w3c.ValidatorBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.ws.rs.core.UriBuilder;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
