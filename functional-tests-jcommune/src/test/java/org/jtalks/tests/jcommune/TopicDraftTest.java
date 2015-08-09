6c82d2c941c78b599112a45c39129734eab21548
Roman Baranov <baranov.r.p@gmail.com>
2015-08-09 18:26:27 +0200

    #JC-1739 fully implemented QA-2758, QA-2759, QA-2774, QA-2768, QA-2780, QA-2802; update logic of other TCs

--- functional-tests-jcommune/src/test/java/org/jtalks/tests/jcommune/TopicDraftTest.java
+++ functional-tests-jcommune/src/test/java/org/jtalks/tests/jcommune/TopicDraftTest.java
@@ -1,29 +1,30 @@
+
 package org.jtalks.tests.jcommune;
 
-import junit.framework.Assert;
 import org.jtalks.tests.jcommune.webdriver.action.Topics;
 import org.jtalks.tests.jcommune.webdriver.action.Users;
 import org.jtalks.tests.jcommune.webdriver.entity.topic.Draft;
-import org.jtalks.tests.jcommune.webdriver.entity.topic.Post;
 import org.jtalks.tests.jcommune.webdriver.entity.topic.Topic;
 import org.jtalks.tests.jcommune.webdriver.entity.user.User;
 import org.jtalks.tests.jcommune.webdriver.exceptions.ValidationException;
 import org.jtalks.tests.jcommune.webdriver.page.Pages;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-import static org.apache.commons.lang.RandomStringUtils.randomAlphanumeric;
+import static org.assertj.core.api.StrictAssertions.assertThat;
 import static org.jtalks.tests.jcommune.webdriver.JCommuneSeleniumConfig.driver;
 import static org.jtalks.tests.jcommune.webdriver.page.Pages.mainPage;
+import static org.jtalks.tests.jcommune.webdriver.page.Pages.postPage;
 
 /**
  * Created by @baranov.r.p
  */
 public class TopicDraftTest {
 
-    private final String BRANCH_CLASSICAL_MECHANICS = "Classical Mechanics";
-    private final String BRANCH_NOTIFICATION_TESTS = "Notification tests";
+    private final String BRANCH_BICYCLE = "Bicycle";
+    private final String BRANCH_EUROPE = "Europe";
 
     @BeforeMethod(alwaysRun = true)
     @Parameters({"appUrl"})
@@ -32,211 +33,197 @@ public class TopicDraftTest {
         mainPage.logOutIfLoggedIn(driver);
     }
 
-    @Test(groups = {"ui-tests", "primary"})
+    // Only business logic
     public void autoSaveDraft_DuringTyping_ShouldSaveIn15Sec_QA2761() {
         Users.signUpAndSignIn();
 
         Topic topic = new Topic();
         Topic createdTopic = Topics.createTopic(topic);
-        Draft draft = (Draft) Topics.typeAnswerWithinMilSec(createdTopic, 15000);
+        Draft draft = Topics.typeAnswerWithinMilSec(createdTopic, 15000);
 
-        Pages.reloadPage(driver);
-
-        Assert.assertTrue(Topics.isDraftCreated(draft));
+        assertThat(draft.getPostContent()).isEqualTo(postPage.findDraftContent());
     }
 
-    @Test(groups = {"ui-tests", "primary"})
+    // bug, draft are not shown after reloading, it's shown only after the second reloading
     public void autoSaveDraft_ReloadPage_ShouldSave_QA2757() {
         Users.signUpAndSignIn();
 
         Topic topic = new Topic();
         Topic createdTopic = Topics.createTopic(topic);
-        Draft draft = (Draft) Topics.typeAnswer(createdTopic);
+        Draft draft = Topics.typeAnswer(createdTopic).reloadPage();
 
-        Pages.reloadPage(driver);
-
-        Assert.assertTrue(Topics.isDraftCreated(draft));
+        assertThat(draft.getPostContent()).isEqualTo(postPage.findDraftContent());
     }
 
-    @Test(groups = {"ui-tests", "primary"})
-    public void autoSaveDraft_OpentherApps_ShouldSave_QA2758() {
+    // Only business logic
+    public void autoSaveDraft_OpenOtherApps_ShouldSave_QA2758() {
         Users.signUpAndSignIn();
 
         Topic topic = new Topic();
         Topic createdTopic = Topics.createTopic(topic);
-        Draft draft = (Draft) Topics.typeAnswer(createdTopic);
+        Draft draft = Topics.typeAnswer(createdTopic);
 
         // there is should be action(s)
         // what does it mean open other application?
 
-        Assert.assertTrue(Topics.isDraftCreated(draft));
+        Assert.assertTrue(Topics.isDraftCreated());
     }
 
-    @Test(groups = {"ui-tests", "primary"})
+@Test(groups = {"ui-tests", "primary"})
     public void autoSaveDraft_MoveFocus_ShouldSave_QA2758() {
         Users.signUpAndSignIn();
-
         Topic topic = new Topic();
         Topic createdTopic = Topics.createTopic(topic);
-        Draft draft = (Draft) Topics.typeAnswer(createdTopic);
 
-        draft.loseFocus();
+        Draft draft = Topics.typeAnswer(createdTopic).loseFocus();
 
-        Assert.assertTrue(Topics.isDraftCreated(draft));
+        assertThat(draft.getPostContent()).isEqualTo(postPage.findDraftContent());
     }
 
-    @Test(groups = {"ui-tests", "primary"})
+@Test(groups = {"ui-tests", "primary"})
     public void autoSaveDraft_Logout_ShouldSave_QA2759() {
         User draftCreator = Users.signUpAndSignIn();
 
         Topic topic = new Topic();
         Topic createdTopic = Topics.createTopic(topic);
-        Draft draft = (Draft) Topics.typeAnswer(createdTopic);
+        Draft draft = Topics.typeAnswer(createdTopic);
 
         Users.logOutAndSignIn(draftCreator);
         Topics.openRequiredTopic(createdTopic);
 
-        Assert.assertTrue(Topics.isDraftCreated(draft));
+        assertThat(draft.getPostContent()).isEqualTo(postPage.findDraftContent());
     }
 
-    @Test(groups = {"ui-tests", "primary"})
+    // framework error (elem not found, during moving post
     public void remainDraft_MoveTopic_ShouldRemain_QA2772() {
         User draftCreator = Users.signUpAndSignIn();
 
         Topic topic = new Topic();
         Topic createdTopic = Topics.createTopic(topic);
-        Draft draft = (Draft) Topics.typeAnswer(createdTopic);
+        Draft draft = Topics.typeAnswer(createdTopic).loseFocus();
 
-        Users.signUpAndSignIn();
-        Topics.moveTopic(topic, BRANCH_CLASSICAL_MECHANICS);
+        assertThat(draft.getPostContent()).isEqualTo(postPage.findDraftContent());
+
+        Users.logOutAndSignIn(User.admin());
+        Topics.moveTopic(topic, BRANCH_BICYCLE); // elem not found
 
         Users.logOutAndSignIn(draftCreator);
         Topics.openRequiredTopic(createdTopic);
 
-        Assert.assertTrue(Topics.isDraftCreated(draft));
+        assertThat(draft.getPostContent()).isEqualTo(postPage.findDraftContent());
     }
 
-    @Test(groups = {"ui-tests", "primary"})
+    // Only business logic
     public void remainDraft_CloseTopic_ShouldRemain_QA2773() {
         Users.signUpAndSignIn();
 
         Topic topic = new Topic();
         Topic createdTopic = Topics.createTopic(topic);
-        Draft draft = (Draft) Topics.typeAnswer(createdTopic);
+        Draft draft = Topics.typeAnswer(createdTopic).loseFocus();
         Topics.closeTopic(); // user should have permissions to close topic
 
-        Pages.reloadPage(driver);
-
-        Assert.assertTrue(Topics.isDraftCreated(draft));
+        assertThat(draft.getPostContent()).isEqualTo(postPage.findDraftContent());
     }
 
-    @Test(groups = {"ui-tests", "primary"})
+@Test(groups = {"ui-tests", "primary"})
     public void autoSaveDraft_ContentWith2Symb_ShouldSave_QA2774() {
-        Users.signUpAndSignIn();
+        int quantitySymb = 2;
 
+        Users.signUpAndSignIn();
         Topic topic = new Topic();
         Topic createdTopic = Topics.createTopic(topic);
-        Draft draft = (Draft) Topics.typeAnswer(createdTopic, new Post(randomAlphanumeric(2)));
 
-        Pages.reloadPage(driver);
+        Draft draft = Topics.typeAnswerCustomLength(createdTopic, quantitySymb).loseFocus();
 
-        Assert.assertTrue(Topics.isDraftCreated(draft));
+        assertThat(draft.getPostContent()).isEqualTo(postPage.findDraftContent());
     }
 
-    @Test(groups = {"ui-tests", "primary"})
+    // Unresponsive Script Error
     public void autoSaveDraft_ContentWith20kSymb_ShouldSave_QA2775() {
-        Users.signUpAndSignIn();
+        int quantitySymb = 20000;
 
+        Users.signUpAndSignIn();
         Topic topic = new Topic();
         Topic createdTopic = Topics.createTopic(topic);
-        Draft draft = (Draft) Topics.typeAnswer(createdTopic, new Post(randomAlphanumeric(20000)));
 
-        Pages.reloadPage(driver);
+        Draft draft = Topics.typeAnswerCustomLength(createdTopic, quantitySymb).loseFocus();
 
-        Assert.assertTrue(Topics.isDraftCreated(draft));
+        assertThat(draft.getPostContent()).isEqualTo(postPage.findDraftContent());
     }
 
-    @Test(groups = {"ui-tests", "primary"})
+    // Only business logic
     public void autoSaveDraft_ContentWith1Symb_ShouldNotSave_QA2791() {
+        int quantitySymb = 1;
         Users.signUpAndSignIn();
 
         Topic topic = new Topic();
         Topic createdTopic = Topics.createTopic(topic);
-        Draft draft = (Draft) Topics.typeAnswer(createdTopic, new Post(randomAlphanumeric(1)));
+        Draft draft = Topics.typeAnswerCustomLength(createdTopic, quantitySymb).loseFocus();
 
-        Pages.reloadPage(driver);
-
-        Assert.assertFalse(Topics.isDraftCreated(draft));
+        Assert.assertFalse(Topics.isDraftCreated());
     }
 
-    @Test(groups = {"ui-tests", "primary"})
+    // Only business logic
     public void autoSaveDraft_ContentOver20kSymb_ShouldNotSave_QA2792() {
+        int quantitySymb = 20001;
+
         Users.signUpAndSignIn();
 
         Topic topic = new Topic();
         Topic createdTopic = Topics.createTopic(topic);
-        Draft draft = (Draft) Topics.typeAnswer(createdTopic, new Post(randomAlphanumeric(20001)));
+        Draft draft = Topics.typeAnswerCustomLength(createdTopic, quantitySymb).loseFocus();
 
-        Pages.reloadPage(driver);
-
-        Assert.assertFalse(Topics.isDraftCreated(draft));
+        Assert.assertFalse(Topics.isDraftCreated());
     }
 
-    @Test(groups = {"ui-tests", "primary"})
+    // Only business logic
     public void autoSaveDraft_ContentCopyPast_ShouldSave_QA2779() {
         Users.signUpAndSignIn();
 
         Topic topic = new Topic();
         Topic createdTopic = Topics.createTopic(topic);
-        Draft draft = (Draft) Topics.pasteAnswer(createdTopic);
+        Draft draft = Topics.pasteAnswer(createdTopic).loseFocus();
 
-        Pages.reloadPage(driver);
-
-        Assert.assertFalse(Topics.isDraftCreated(draft));
+        assertThat(draft.getPostContent()).isEqualTo(postPage.findDraftContent());
     }
 
-    @Test(groups = {"ui-tests", "primary"})
+@Test(groups = {"ui-tests", "primary"})
     public void autoSaveDraft_DifferentUsersSameTopic_ShouldSaveBoth_QA2768() {
         User firstUser = Users.signUpAndSignIn();
 
         Topic topic = new Topic();
         Topic createdTopic = Topics.createTopic(topic);
-        Draft firstDraft = (Draft) Topics.pasteAnswer(createdTopic);
+        Draft firstDraft = Topics.pasteAnswer(createdTopic).loseFocus();
 
-        Pages.reloadPage(driver);
-
-        Assert.assertTrue(Topics.isDraftCreated(firstDraft));
+        assertThat(firstDraft.getPostContent()).isEqualTo(postPage.findDraftContent());
 
         Users.logout();
-        User secondUser = Users.signUpAndSignIn();
+        Users.signUpAndSignIn();
         Topics.openRequiredTopic(createdTopic);
-        Draft secondDraft = (Draft) Topics.pasteAnswer(createdTopic);
+        Draft secondDraft = Topics.pasteAnswer(createdTopic).loseFocus();
 
-        Pages.reloadPage(driver);
-
-        Assert.assertTrue(Topics.isDraftCreated(secondDraft));
+        assertThat(secondDraft.getPostContent()).isEqualTo(postPage.findDraftContent());
 
         Users.logOutAndSignIn(firstUser);
         Topics.openRequiredTopic(createdTopic);
 
-        Assert.assertFalse(Topics.isDraftCreated(firstDraft));
-
+        assertThat(firstDraft.getPostContent()).isEqualTo(postPage.findDraftContent());
     }
 
-    @Test(groups = {"ui-tests", "primary"})
+@Test(groups = {"ui-tests", "primary"})
     public void autoSaveDraft_DifferentDraftInDifferentBranches_ShouldSave_QA2780() {
         Users.signUpAndSignIn();
 
-        Topic topicFirst = Topics.createTopic(new Topic().withBranch(BRANCH_NOTIFICATION_TESTS));
-        Draft draftFirst = (Draft) Topics.typeAnswer(topicFirst);
-        Topic topicSecond = Topics.createTopic(new Topic().withBranch(BRANCH_NOTIFICATION_TESTS));
-        Draft draftSecond = (Draft) Topics.typeAnswer(topicSecond);
+        Topic topicFirst = Topics.createTopic(new Topic().withBranch(BRANCH_EUROPE));
+        Draft draftFirst = Topics.typeAnswer(topicFirst).loseFocus();
+        Topic topicSecond = Topics.createTopic(new Topic().withBranch(BRANCH_BICYCLE));
+        Draft draftSecond = Topics.typeAnswer(topicSecond).loseFocus();
 
         Topics.openRequiredTopic(topicFirst);
-        Assert.assertTrue(Topics.isDraftCreated(draftFirst));
+        assertThat(draftFirst.getPostContent()).isEqualTo(postPage.findDraftContent());
 
         Topics.openRequiredTopic(topicSecond);
-        Assert.assertTrue(Topics.isDraftCreated(draftSecond));
+        assertThat(draftSecond.getPostContent()).isEqualTo(postPage.findDraftContent());
     }
 
     @Test(groups = {"ui-tests", "primary"})
@@ -245,18 +232,18 @@ public class TopicDraftTest {
 
         Topic topic = new Topic();
         Topic createdTopic = Topics.createTopic(topic);
-        Draft draft = (Draft) Topics.typeAnswer(createdTopic);
-        Pages.reloadPage(driver);
-        Assert.assertTrue(Topics.isDraftCreated(draft));
+        Draft draft = Topics.typeAnswer(createdTopic).loseFocus();
 
-        Users.clearCookiesAndLogout();
+        assertThat(draft.getPostContent()).isEqualTo(postPage.findDraftContent());
+
+        Users.deleteCookiesAndLogout();
         Users.signIn(user);
         Topics.openRequiredTopic(createdTopic);
 
-        Assert.assertTrue(Topics.isDraftCreated(draft));
+        assertThat(draft.getPostContent()).isEqualTo(postPage.findDraftContent());
     }
 
-    @Test(groups = {"ui-tests", "primary"})
+    // Only business logic
     public void autoSaveDraft_ContentEmpty_ShouldNotSave_QA2767() {
         String spaceSymb = " ";
 
@@ -264,71 +251,53 @@ public class TopicDraftTest {
 
         Topic topic = new Topic();
         Topic createdTopic = Topics.createTopic(topic);
-        Draft draft = (Draft) Topics.typeAnswer(createdTopic, new Post(spaceSymb));
+//        Draft draft = Topics.typeAnswer(createdTopic, spaceSymb);
 
         Pages.reloadPage(driver);
 
-        Assert.assertFalse(Topics.isDraftCreated(draft));
+        Assert.assertFalse(Topics.isDraftCreated());
     }
 
-    @Test(groups = {"ui-tests", "primary"})
+    // Only business logic
     public void removeDraft_PublishPost_ShouldRemove_QA2777() {
         Users.signUpAndSignIn();
 
         Topic topic = new Topic();
         Topic createdTopic = Topics.createTopic(topic);
-        Draft draft = (Draft) Topics.typeAnswer(createdTopic);
-        draft.loseFocus();
+        Draft draft = Topics.typeAnswer(createdTopic).loseFocus();
 
         draft.publish();
         Pages.reloadPage(driver);
 
-        Assert.assertFalse(Topics.isDraftCreated(draft));
+        Assert.assertFalse(Topics.isDraftCreated());
     }
 
-    @Test(groups = {"ui-tests", "primary"})
-    public void removeDraft_ClickTrashIcon_ShouldRemove_QA2763() {
-        Users.signUpAndSignIn();
-
-        Topic topic = new Topic();
-        Topic createdTopic = Topics.createTopic(topic);
-        Draft draft = (Draft) Topics.typeAnswer(createdTopic);
-        draft.loseFocus();
-
-        draft.remove();
-        Pages.reloadPage(driver);
-
-        Assert.assertFalse(Topics.isDraftCreated(draft));
-    }
-
-    @Test(groups = {"ui-tests", "primary"})
+    // Only business logic
     public void removeDraft_DeleterContent_ShouldRemove_QA2765() {
         Users.signUpAndSignIn();
 
         Topic topic = new Topic();
         Topic createdTopic = Topics.createTopic(topic);
-        Draft draft = (Draft) Topics.typeAnswer(createdTopic);
-        draft.loseFocus();
+        Draft draft = Topics.typeAnswer(createdTopic).loseFocus();
 
         draft.deleteContent();
         Pages.reloadPage(driver);
 
-        Assert.assertFalse(Topics.isDraftCreated(draft));
+        Assert.assertFalse(Topics.isDraftCreated());
     }
 
-    @Test(groups = {"ui-tests", "primary"})
+
     public void autoSaveDraft_AfterEditRemain1Symb_ShouldSave_QA2778() {
         int quntitySymb = 1;
         Users.signUpAndSignIn();
 
         Topic topic = new Topic();
         Topic createdTopic = Topics.createTopic(topic);
-        Draft draft = (Draft) Topics.typeAnswer(createdTopic);
-        draft.loseFocus();
+        Draft draft = Topics.typeAnswer(createdTopic).loseFocus();
 
         draft.editContent(quntitySymb);
         Pages.reloadPage(driver);
 
-        Assert.assertFalse(Topics.isDraftCreated(draft));
+        assertThat(draft.getPostContent()).isEqualTo(postPage.findDraftContent());
     }
 }
